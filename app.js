43ace7fd2e07e09b80a4905c6ae788801f42a135
AHE54 <alan.heffernan@sky.uk>
2020-04-14 13:23:08 +0100

    updated Js file

--- app.js
+++ app.js
@@ -3,30 +3,44 @@ let quoteAuthor = document.getElementById('quoteAuthor');
 let quoteGenerator = document.getElementById('newQuoteButton');
 
 let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-let inspireUrl = `${proxyUrl}https://quotes.rest/qod?category=inspire`;
 let forismaticQuote = `${proxyUrl}https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json`;
 
-//Find ramdon quote generator and call and retrieve response
-window.onload = function() {
-     this.getQuote(forismaticQuote);
+window.onload = function () {
+      this.getQuote(forismaticQuote);
 }
 
 quoteGenerator.addEventListener("click", changeQuote);
 
-function getQuote(url){
-fetch(forismaticQuote)
-.then(response => {
-    return response.json();
-})
-.then(data => {
-    console.log(data);
-    console.log(data.quoteText);
-
-    quoteText.textContent = data.quoteText;
-    quoteAuthor.textContent = "- " + data.quoteAuthor;
-});
+function changeQuote() {
+    
+    let apiResponse = getQuote(forismaticQuote);
+    console.log(apiResponse);
+    if(apiResponse == null){
+        getStaticQuote();
+    }
 }
 
-function changeQuote() {
-    getQuote(forismaticQuote);
+function getQuote(url) {
+    fetch(forismaticQuote)
+        .then(response => {
+            if(response.status >= 200 && response.status <= 299){
+            return response.json();
+        }else {
+            throw Error(response.statusText);
+        }
+        })
+        .then(data => {
+            quoteText.textContent = data.quoteText;
+            quoteAuthor.textContent = "- " + data.quoteAuthor;
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+function getStaticQuote(){
+        let randomNumber = Math.floor(Math.random() * Math.floor(quoteArray.length));
+        quoteText.textContent = quoteArray[randomNumber].quote;
+        quoteAuthor.textContent = quoteArray[randomNumber].author;
+
 }
